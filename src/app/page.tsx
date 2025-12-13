46e1a6f439726c4246b9b8da6543d5c0b6389414
GuDoYoon <rhehfl418q@gmail.com>
2025-12-14 03:42:48 +0900

    Refactor code structure for improved readability and maintainability

---
+++ src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div>하이하이</div>;
+}
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { Button } from '@/components/common/Button';
+import Link from 'next/link';
+import { useState } from 'react';
+
+export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  return (
+    <header className="w-full flex py-9 justify-between px-10">
+      <div>로고</div>
+      <nav className="hidden md:flex">
+        <ul className="flex space-x-4">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <button
+        className="md:hidden text-2xl"
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      >
+        {isMenuOpen ? '✕' : '☰'}
+      </button>
+      {isMenuOpen && (
+        <div className="md:hidden flex flex-col gap-4 mt-4 p-4 bg-gray-50 rounded-lg">
+          <a href="#about" className="block py-2">
+            About
+          </a>
+          <a href="#projects" className="block py-2">
+            Projects
+          </a>
+          <Button label="Contact" />
+        </div>
+      )}
+    </header>
+  );
+}
