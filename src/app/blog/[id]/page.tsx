543463d3144ec1d88f9d072e88a73a6525653531
GuDoYoon <rhehfl418q@gmail.com>
2026-01-23 01:47:50 +0900

    feat: blog생성

---
+++ src/app/blog/[id]/page.tsx
@@ -0,0 +1,24 @@
+import MarkDownWrapper from '@/components/common/MarkDownWrapper';
+import { supabase } from '@/lib/supabase';
+import { notFound } from 'next/navigation';
+
+export default async function BlogPost({ params }: { params: { id: string } }) {
+  const { id } = await params;
+  const { data: post } = await supabase
+    .from('posts')
+    .select('*')
+    .eq('id', id)
+    .single();
+
+  if (!post) notFound();
+
+  return (
+    <article className="max-w-3xl mx-auto py-10 prose lg:prose-xl">
+      <h1>{post.title}</h1>
+      <div className="text-gray-500 mb-8">
+        {new Date(post.created_at).toLocaleDateString()} 작성
+      </div>
+      <MarkDownWrapper>{post.content}</MarkDownWrapper>
+    </article>
+  );
+}
