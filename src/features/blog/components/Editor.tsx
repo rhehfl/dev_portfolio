3e8c275bd60582ca3bf74942a2acbd616e74cecc
GuDoYoon <rhehfl418q@gmail.com>
2026-03-02 14:19:25 +0900

    feat: 에디터에서 본문 입력 필드의 defaultValue를 value로 변경

--- src/features/blog/components/Editor.tsx
+++ src/features/blog/components/Editor.tsx
@@ -62,7 +62,7 @@ export default function Editor({ postId }: EditorUIProps) {
             ref={textareaRef}
             placeholder="본문을 입력하세요"
             className="w-full h-full resize-none overflow-hidden outline-none bg-transparent text-lg leading-relaxed text-foreground"
-            defaultValue={post.content}
+            value={post.content}
             onChange={handleContentChange}
             onPaste={handlePaste}
           />
